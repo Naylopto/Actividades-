55cd3bf9514ca7dee886cc2a39787917a1f80562
Naylopto <alondranayelilopeztorres@gmail.com>
2024-01-08 18:48:04 -0600

    avances

--- VariablesACT.js
+++ VariablesACT.js
@@ -17,5 +17,40 @@
 * Declarar 5 variables y convertirlas a otro tipo de dato
 * 
 *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
-* Imprimir los resultados en pantalla con un console.log()
+* Imprimir los resultados en pantalla con un console.log() */
+
+let number0 = 0;
+let number1 = 1;
+let number2 = 2;
+let number3 = 3;
+let number4 = 4;
+let number5 = 5;
+let number6 = 6;
+let number7 = 7;
+let number8 = 8;
+let number9 = 9;
+
+let cadena1 = ("¡HOLA!")
+let cadena2 = (" ESTA ES LA PRIMER ACTIVIDAD ")
+let cadena3 = ("EN JS, ME SIENTO PERDIDA, PERDIDA, PERDIDAAAA!")
+let cadena4 = (" y si pongo esto en japo, igual no tengo  un limite")
+let cadena5 = (" Maigo ni natta yo, maigo, maigooo!!! ")
+
+// operaciones basicas
+let suma = number4 + number9 ;
+let resta = number1 - number5; 
+let multiplicacion = number8 * number3;
+let division =  number6 / number2;
+
+// resultados operaciones 
+
+console.log("Este es el resultado de la suma" + suma);
+console.log("Este es el resultado de la resta" + resta);
+console.log("Este es el resultado de la multiplicacion" + multiplicacion);
+console.log("Este es el resultado de la division" + division);
+
+// concatenacion
+
+console.log(cadena1 + cadena2 + cadena3 + cadena4 + cadena5);
+
 
