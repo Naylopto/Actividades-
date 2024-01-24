8fc8e7df5061575c242e66a31fb4d02137d3207a
Naylopto <alondranayelilopeztorres@gmail.com>
2024-01-24 13:06:30 -0600

    tarea act

---
+++ tareaReact/my-react-app/src/Components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <div>
+            <h1>Sobre me</h1>
+            <img src="https://i.pinimg.com/474x/d1/80/9f/d1809f821b85df526e78901426d47cbc.jpg" alt="niña con ojeras" />
+            <p>Nombre: Alondra Nayeli Lopez Torres</p>
+            <p>Correo: alondranayelilt@gmail.com </p>
+            <p>Descripcion: Mi nombre esta arriba, pero me gusta que me digan naye (porqué cuando me dicen Alondra o Nayeli siento que me regañan)
+                tengo 2.. años.
+                tengo una gatita llamada Teodora, ella esta loca. </p>
+        </div>
+    )
+}
+
+export default About;
